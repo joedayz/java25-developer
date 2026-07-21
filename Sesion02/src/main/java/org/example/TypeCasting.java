b1ea4799a0843aa5e16068f1d79818738a6210c5
Jose Diaz <jose.diaz@joedayz.pe>
2026-07-20 19:44:27 -0500

    type casting

---
+++ Sesion02/src/main/java/org/example/TypeCasting.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class TypeCasting {
+
+    public static void main(String[] args) {
+        byte a = 121, b =6;
+        byte c = (byte)(a + b);
+        int aInt = a + b;
+        int bInt = a/b;
+        float fNum = a/b;  // 20.0F
+
+        byte x  = 121 + 6;  // 127
+
+        System.out.println("fNum " + fNum);
+
+        float f2Num =  (float)(a/b);
+        System.out.println("f2Num " + f2Num);
+
+        float f3Num =  (float)a/b;
+        System.out.println("f3Num " + f3Num);
+
+        float f4Num = a/(float) b;
+        System.out.println("f4Num " + f4Num);
+
+
+        b = (byte)(b + 1);
+
+        float f1 = 25.0f, f2 = 13.0f;
+
+        float result = f1 + f2;
+        System.out.println("result " + result);
+
+        float f3 = 25.0f;
+        float result2 = (float) (13.0 + f3);
+        System.out.println("result2 " + result2);
+
+        float resultado = bInt + f1 + x;  //  int  + float + byte
+        System.out.println("resultado " + resultado);
+
+    }
+}
