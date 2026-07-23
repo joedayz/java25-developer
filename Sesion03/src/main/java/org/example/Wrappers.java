921f98931ab73c9311f08fc2bfc08f68629ae125
Jose Diaz <jose.diaz@joedayz.pe>
2026-07-22 20:29:02 -0500

    clases Wrappers

---
+++ Sesion03/src/main/java/org/example/Wrappers.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Wrappers {
+
+    public static void main(String[] args) {
+        int a = 42;
+        Integer b = Integer.valueOf(a); // Boxing
+        int c = b.intValue(); // Unboxing
+
+        b = a;
+        c = b;
+
+        String d = "12.25";
+        Float e = Float.valueOf(d); // Boxing
+        float f = e.floatValue(); // Unboxing
+        float f2 = Float.parseFloat(d);
+
+        String g = String.valueOf(a);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+
+        Integer x = 100;  // Boxing
+        int y = b; // Unboxing
+    }
+}
