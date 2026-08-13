fd56bc0a9c531ff564dbcfbe61392e890608196a
Jose Diaz <jose.diaz@joedayz.pe>
2026-08-12 19:45:26 -0500

    pase por copia

--- Session04/src/main/java/com/acme/enumeration/Product.java
+++ Session04/src/main/java/com/acme/enumeration/Product.java
@@ -1,8 +1,11 @@
 package com.acme.enumeration;
 
+import java.math.BigDecimal;
+
 public class Product {
 
     private String name;
+    private BigDecimal price;
     private String caution;
     private Condition condition;
 
@@ -10,6 +13,11 @@ public class Product {
         this.name = name;
         this.condition = condition;
     }
+
+    public Product() {
+
+    }
+
     public Product serve(){
         switch (condition){
             case HOT -> this.addCaution("Caution: This product is hot!");
@@ -24,4 +32,33 @@ public class Product {
         return this;
     }
 
+    public void manageProduct(){
+        Product p1 = new Product();
+        orderProduct(p1);
+        double v1 = p1.getPrice();
+        changePrice(v1);
+        System.out.println("Price after changePrice: " + v1);
+    }
+
+    private void changePrice(double v1) {
+        v1 = 2.99;
+    }
+
+    private void orderProduct(Product p1) {
+        p1.setPrice(BigDecimal.valueOf(1.99));
+    }
+
+    private void setPrice(BigDecimal bigDecimal) {
+        this.price = bigDecimal;
+    }
+
+
+    public double getPrice() {
+        return price.doubleValue();
+    }
+
+    static void main() {
+        Product p1 = new Product();
+        p1.manageProduct();
+    }
 }
