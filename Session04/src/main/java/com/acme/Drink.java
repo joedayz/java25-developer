5bb8a48b37ead7b2d8c64bed9bd3f336d93b6c48
Jose Diaz <jose.diaz@joedayz.pe>
2026-08-19 20:14:08 -0500

    sobrescribiendo en Drink

--- Session04/src/main/java/com/acme/Drink.java
+++ Session04/src/main/java/com/acme/Drink.java
@@ -1,4 +1,18 @@
 package com.acme;
 
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
 public class Drink extends Product{
+
+
+    @Override
+    public BigDecimal getPrice() {
+        LocalTime now = LocalTime.now();
+        BigDecimal discount = (now.isAfter(LocalTime.of(17, 30))
+                && now.isBefore(LocalTime.of(18, 30)))
+                ? super.getPrice().multiply(BigDecimal.valueOf(0.2))
+                : BigDecimal.ZERO;
+        return super.getPrice().subtract(discount);
+    }
 }
